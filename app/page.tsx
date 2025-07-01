29d19660b19f84e5446ba63340ecf2073adf2015
Bolaji87 <sakabolaji77@gmail.com>
2025-07-01 15:19:16 +0100

    feat:change background colour of main component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ function Page() {
   return (
     <div className="bg-teal-700 min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow flex justify-center items-start p-6">
+      <main className="flex-1  bg-gradient-to-r from-blue-400 via-blue-300 to-sky-300 flex justify-center items-start p-6">
         <WeatherForm />
       </main>
     </div>
